c5b60ebd20bd6661552db15c14a3d49a98b4833f
Roberto Albino <junior_wash@hotmail.com>
2018-03-30 08:45:16 -0300

    Removing the applicationwebxml.java

--- src/main/java/com/roberto/controle/repository/PersistentTokenRepository.java
+++ src/main/java/com/roberto/controle/repository/PersistentTokenRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 /**
  * Spring Data JPA repository for the PersistentToken entity.
  */
-public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+public interface
+PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
 
     List<PersistentToken> findByUser(User user);
 
