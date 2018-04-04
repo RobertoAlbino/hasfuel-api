8865ecd00af70c4d0635a18299b41b91c681eb7f
Roberto Albino Junior <roberto.junior@sistemainfo.corp>
2018-04-04 13:27:44 -0300

    Criando a estrutura de testes no projeto

---
+++ src/test/java/TesteTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteTest {
+
+    @Test
+    public void testar() {
+        Assert.assertTrue(2 == 2);
+    }
+
+    @Test
+    public void testar2() {
+        Assert.assertTrue( 1 == 2);
+    }
+}
