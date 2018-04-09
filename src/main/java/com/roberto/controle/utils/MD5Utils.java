76eb45c5d1d04ff3b2164a47fa040086aa8e88ba
Roberto Albino Junior <roberto.junior@sistemainfo.corp>
2018-04-09 13:28:29 -0300

    Adicionando classe de utilidades para trabalhar com MD5

---
+++ src/main/java/com/roberto/controle/utils/MD5Utils.java
@@ -0,0 +1,10 @@
+package com.roberto.controle.utils;
+
+import java.security.*;
+import java.math.*;
+
+public class MD5Utils {
+    public static String encriptarSenha(String senha) {
+        return "";
+    }
+}
