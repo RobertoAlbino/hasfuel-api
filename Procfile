c7ffb8c9cc025cbcdabecfb9257823af7af6661c
Roberto Albino Junior <junior_wash@hotmail.com>
2018-04-09 17:45:12 -0300

    Deploy Controle Java Backend Heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT -jar target/spring-boot-heroku.jar
--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
 git add --all
-git commit -m "Deploy"
-git push heroku master
+git commit -m "Deploy Controle Java Backend Heroku"
+gradlew build
+heroku war:deploy --app controle-java-backend C:\Projetos\Controle-java-backend\build\libs\controle-0.0.1-SNAPSHOT.war
 pause
 
