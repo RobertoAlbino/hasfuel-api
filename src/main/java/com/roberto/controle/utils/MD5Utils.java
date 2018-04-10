2fdbd3d7ecb7c69c5cbc945b7de7afb7a598b788
Roberto Albino Junior <junior_wash@hotmail.com>
2018-04-10 18:02:14 -0300

    Implementando o md5utils

--- src/main/java/com/roberto/controle/utils/MD5Utils.java
+++ src/main/java/com/roberto/controle/utils/MD5Utils.java
@@ -1,10 +1,16 @@
 package com.roberto.controle.utils;
 
 import java.security.*;
-import java.math.*;
 
 public class MD5Utils {
-    public static String encriptarSenha(String senha) {
-        return "";
+    private MessageDigest messageDigest;
+
+    public MD5Utils() {
+
+    }
+    public static byte[] encriptarSenha(String senha) throws Exception {
+         = MessageDigest.getInstance("MD5");
+        byte[] stringBytes = senha.getBytes("UTF-8");
+        return md.digest(stringBytes);
     }
 }
