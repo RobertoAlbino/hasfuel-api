53be6911d161d266261026b1740dfb5b28cd561c
Roberto Albino Junior <junior_wash@hotmail.com>
2018-04-18 13:33:28 -0300

    Implementando a classe genérica para geração de senhas MD5

--- src/main/java/com/roberto/controle/utils/MD5Utils.java
+++ src/main/java/com/roberto/controle/utils/MD5Utils.java
@@ -1,16 +1,13 @@
 package com.roberto.controle.utils;
 
+
 import java.security.*;
 
 public class MD5Utils {
-    private MessageDigest messageDigest;
 
-    public MD5Utils() {
-
-    }
     public static byte[] encriptarSenha(String senha) throws Exception {
-         = MessageDigest.getInstance("MD5");
-        byte[] stringBytes = senha.getBytes("UTF-8");
-        return md.digest(stringBytes);
+        return MessageDigest.getInstance("MD5").digest(senha.getBytes("UTF-8"));
     }
+
+
 }
