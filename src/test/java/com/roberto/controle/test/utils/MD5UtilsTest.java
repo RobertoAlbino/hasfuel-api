644a440d90177dedcfe8f8a49bcbc64d83bcaa47
Roberto Albino <junior_wash@hotmail.com>
2018-04-25 23:27:43 -0300

    Adicionando alguns testes de unidade

--- src/test/java/com/roberto/controle/test/utils/MD5UtilsTest.java
+++ src/test/java/com/roberto/controle/test/utils/MD5UtilsTest.java
@@ -7,6 +7,16 @@ import com.roberto.controle.utils.MD5Utils;
 public class MD5UtilsTest {
 
     @Test
+    public void garantirTamanhoMD5Maior10() throws Exception {
+        Assert.assertTrue(MD5Utils.encriptarSenha("Roberto").length() > 10);
+    }
+
+    @Test
+    public void garantirTamanhoMD5Menor60() throws Exception {
+        Assert.assertTrue(MD5Utils.encriptarSenha("Roberto").length() < 60);
+    }
+
+    @Test
     public void garantirQueUmaStringSempreGeraMesmoMD5() throws Exception {
         Assert.assertEquals(MD5Utils.encriptarSenha("Roberto"),MD5Utils.encriptarSenha("Roberto"));
     }
