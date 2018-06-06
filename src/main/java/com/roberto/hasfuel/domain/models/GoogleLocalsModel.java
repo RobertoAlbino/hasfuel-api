e82679b1ba98f85efc5c00410d743a75b33fc4b4
Roberto Albino Junior <junior_wash@hotmail.com>
2018-06-06 13:29:52 -0300

    Iniciando a modelagem do objeto de retorno da Google

---
+++ src/main/java/com/roberto/hasfuel/domain/models/GoogleLocalsModel.java
@@ -0,0 +1,50 @@
+package com.roberto.hasfuel.domain.models;
+
+public class GoogleLocalsModel {
+
+    private String id;
+    private String icon;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
